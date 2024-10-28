74c3a6fc8f04940be2564f79a318cbaedf9ccc8f
wafaa alomour <wafaa.alomour@gmail.com>
2024-10-28 14:04:11 +0200

    add data models and migration

---
+++ RestaurantReservation.Db/RestaurantReservationDbContext.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RestaurantReservation.Db
+{
+    public class RestaurantReservationDbContext : DbContext
+    {
+        // DbSets for each entity go here, e.g.:
+
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Reservation> Reservations { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<MenuItem> MenuItems { get; set; }
+        public DbSet<Table> Tables { get; set; }
+        public DbSet<Restaurant> Restaurants { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                "Data Source=(LocalDb)\\MSSQLLocalDB;Initial Catalog=RestaurantReservationCore");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Customer to Reservation relationship
+            modelBuilder.Entity<Reservation>()
+                .HasOne(r => r.Customer)
+                .WithMany(c => c.Reservations)
+                .HasForeignKey(r => r.CustomerId);
+
+            // Restaurant to Table relationship
+            modelBuilder.Entity<Table>()
+                .HasOne(t => t.Restaurant)
+                .WithMany(r => r.Tables)
+                .HasForeignKey(t => t.RestaurantId);
+
+            // Restaurant to Employee relationship
+            modelBuilder.Entity<Employee>()
+                .HasOne(e => e.Restaurant)
+                .WithMany(r => r.Employees)
+                .HasForeignKey(e => e.RestaurantId);
+
+            // Restaurant to MenuItem relationship
+            modelBuilder.Entity<MenuItem>()
+                .HasOne(m => m.Restaurant)
+                .WithMany(r => r.MenuItems)
+                .HasForeignKey(m => m.RestaurantId);
+
+            // Reservation to Table relationship
+            modelBuilder.Entity<Reservation>()
+                .HasOne(r => r.Table)
+                .WithMany(t => t.Reservations)
+                .HasForeignKey(r => r.TableId);
+
+            // Order to Reservation relationship
+            modelBuilder.Entity<Order>()
+                .HasOne(o => o.Reservation)
+                .WithMany(r => r.Orders)
+                .HasForeignKey(o => o.ReservationId);
+
+            // Order to Employee relationship
+            modelBuilder.Entity<Order>()
+                .HasOne(o => o.Employee)
+                .WithMany(e => e.Orders)
+                .HasForeignKey(o => o.EmployeeId);
+
+            // OrderItem to Order relationship
+            modelBuilder.Entity<OrderItem>()
+                .HasOne(oi => oi.Order)
+                .WithMany(o => o.OrderItems)
+                .HasForeignKey(oi => oi.OrderId);
+
+            // OrderItem to MenuItem relationship
+            modelBuilder.Entity<OrderItem>()
+                .HasOne(oi => oi.MenuItem)
+                .WithMany(m => m.OrderItems)
+                .HasForeignKey(oi => oi.ItemId);
+
+            // Define primary keys
+            modelBuilder.Entity<MenuItem>()
+                .HasKey(m => m.ItemId);
+
+            modelBuilder.Entity<Table>()
+                .HasKey(t => t.TableId);
+
+            modelBuilder.Entity<Reservation>()
+                .HasKey(r => r.ReservationId);
+
+            modelBuilder.Entity<Employee>()
+                .HasKey(e => e.EmployeeId);
+
+            modelBuilder.Entity<Order>()
+                .HasKey(o => o.OrderId);
+
+            modelBuilder.Entity<OrderItem>()
+                .HasKey(oi => oi.OrderItemId);
+
+            modelBuilder.Entity<Customer>()
+                .HasKey(c => c.CustomerId);
+
+            modelBuilder.Entity<Restaurant>()
+                .HasKey(r => r.RestaurantId);
+        }
+    }
+}
